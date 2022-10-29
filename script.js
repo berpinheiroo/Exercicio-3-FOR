be1d1adb305e1e1d07cb67eb5e97c5d5d4e698f0
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-29 20:39:24 -0300

    Mudando de repositorio

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Utilizando o FOR, faça um algoritmo que receba 5 números e conte
+quantos estão entre o intervalo dos números 1 - 10. */
+
+
+var numeros
+var quantidade = 0
+
+for (var contador = 0; contador < 5; contador++) {
+    numeros = parseInt(prompt("Escolha um numero"))
+    if (numeros >= 1 && numero <= 10) {
+        quantidade++
+    }
+   
+}
+
+console.log("A quantidade de numeros entre 1 e 10 e:", quantidade)
+
+
+
+
+
